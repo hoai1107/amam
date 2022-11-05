6a03eb17b218b75e8cc9382e98b194530fa2e9dd
dtrongtvo <tvdtrong@gmail.com>
2022-11-05 13:25:13 +0700

    add basic style class

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { ref } from "vue";
+import ButtonItem from "@/components/ButtonItem.vue";
+
 const name = ref("Hello World");
 </script>
 
 <template>
   <p>{{ name }}</p>
+  <ButtonItem></ButtonItem>
 </template>
---
+++ src/frontend/src/components/ButtonItem.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <button class="xxs-600">Hello</button>
+</template>

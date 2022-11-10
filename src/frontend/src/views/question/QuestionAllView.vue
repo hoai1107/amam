a2a41da19713274d64bebac5717dd2a5d124f745
hoai1107 <khaihoai1107@gmail.com>
2022-11-10 21:47:39 +0700

    Add basic routes and views for frontend

---
+++ src/frontend/src/views/question/QuestionAllView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>All Questions</div>
+  <div>Query: {{ props }}</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: String,
+  author: String,
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/frontend/src/views/question/QuestionSingleView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Single Question</div>
+  <div>Question Id: {{ props.id }}</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  id: String,
+});
+</script>
+
+<style lang="scss" scoped></style>

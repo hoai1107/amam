3eeae8b79a9853c6b83a21e19b018fa45e9bc632
hientoanlam <95153617+hientoanlam@users.noreply.github.com>
2022-11-13 23:25:58 +0700

    create sort menu

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref } from "vue";
 import ButtonItem from "@/components/ButtonItem.vue";
-
+import SortMenu from "@/components/SortMenu.vue";
 const name = ref("Hello World");
 </script>
 
 <template>
   <p>{{ name }}</p>
   <ButtonItem type="primary" state="normal"></ButtonItem>
+  <SortMenu></SortMenu>
 </template>

8fdfae1b565627e78a0b9aa02f803e963c0d220b
hientoanlam <95153617+hientoanlam@users.noreply.github.com>
2022-11-15 11:45:12 +0700

    add search bar component

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import ButtonItem from "@/components/ButtonItem.vue";
 import SortMenu from "@/components/SortMenu.vue";
+import SearchBar from "@/components/SearchBar.vue";
 const name = ref("Hello World");
 </script>
 
@@ -9,4 +10,5 @@ const name = ref("Hello World");
   <p>{{ name }}</p>
   <ButtonItem type="primary" state="normal"></ButtonItem>
   <SortMenu></SortMenu>
+  <SearchBar></SearchBar>
 </template>

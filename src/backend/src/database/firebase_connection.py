660dcdad5b773c2d105b9e7f0e9ce7febf04af83
viethung9at1 <80512263+viethung9at1@users.noreply.github.com>
2022-11-23 17:35:17 +0700

    Firestore database connect

--- src/backend/src/database/firebase_connection.py
+++ src/backend/src/database/firebase_connection.py
@@ -1,15 +1,17 @@
-import collections 
-import sys
-if sys.version_info.major == 3 and sys.version_info.minor >= 10:
-
-    from collections.abc import MutableMapping
-else:
-    from collections import MutableMapping
-from firebase_admin import credentials
-from firebase_admin import auth
 import firebase_admin
-#cred = credentials.RefreshToken('cs300-project-firebase-adminsdk-z5mzh-43da7b3a43.json')
-#default_app = firebase_admin.initialize_app(cred)
-#default_app = firebase_admin.initialize_app(cred)
-default_app = firebase_admin.initialize_app()
-print(default_app.name)
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred=credentials.Certificate('cs300-project-firebase-adminsdk-z5mzh-43da7b3a43.json')
+firebase_admin.initialize_app(cred)
+db=firestore.client()
+obj1={
+  "Name":"Mike",
+  "Age":100,
+  "Net worth":100000
+}
+obj2={
+  "Nmae":"Tony",
+  "Age":10,
+  "New worth":100000
+}
+data=[obj1,obj2]

9a73fce22dce30624f115ec6b718f855e5213a88
hoai1107 <khaihoai1107@gmail.com>
2022-11-29 15:59:07 +0700

    Implement basic layout and sidebar

--- src/frontend/src/components/layout/MainContainer.vue
+++ src/frontend/src/components/layout/MainContainer.vue
@@ -1,44 +1,33 @@
 <template>
-  <div class="w-full h-fit">
+  <div class="w-full h-fit mb-6">
     <div class="w-[1272px] mx-auto">
       <div class="grid grid-cols-12 gap-x-6">
         <div class="col-span-2">
-          <div class="text-xl font-semibold mb-3">Categories</div>
-          <div class="flex flex-col gap-y-2 mb-3">
-            <div v-for="(category, index) in categories" v-bind:key="index">
-              <router-link
-                :to="{
-                  name: 'questions.category',
-                  params: { category: category.toLowerCase() },
-                }"
-                >{{ category }}</router-link
-              >
-            </div>
-          </div>
-          <div class="text-blue">See all</div>
+          <SidebarCategory></SidebarCategory>
         </div>
 
-        <div class="col-span-7 border border-solid">Content</div>
+        <div class="col-span-7">
+          <div class="text-xl font-semibold mb-3">All Questions</div>
+          <div class="flex flex-col gap-y-6">
+            <QuestionCard />
+            <QuestionCard />
+            <QuestionCard />
+            <QuestionCard />
+          </div>
+        </div>
 
-        <div class="col-span-3 border border-solid">Side bar 2</div>
+        <div class="col-span-3">
+          <SidebarHistory></SidebarHistory>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-const categories = [
-  "Technology",
-  "Movies",
-  "Health",
-  "Food",
-  "Music",
-  "Books",
-  "Business",
-  "Psychology",
-  "History",
-  "Cooking",
-];
+import QuestionCard from "@/components/layout/QuestionCard.vue";
+import SidebarCategory from "@/components/layout/SidebarCategory.vue";
+import SidebarHistory from "@/components/layout/SidebarHistory.vue";
 </script>
 
 <style lang="scss" scoped></style>

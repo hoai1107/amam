0e80949e7a772991e9de3ab6174efeca818824b3
hoai1107 <khaihoai1107@gmail.com>
2023-01-10 20:50:57 +0700

    Complete history post

--- src/frontend/src/components/layout/sidebar/SidebarHistory.vue
+++ src/frontend/src/components/layout/sidebar/SidebarHistory.vue
@@ -1,27 +1,43 @@
 <template>
   <div class="text-xl font-semibold mb-3">History</div>
-  <div class="grid grid-cols-1 divide-y divide-gray-100 divide-solid mb-3">
-    <div
-      v-for="post in historyPosts"
-      v-bind:key="post.id"
-      class="py-4 first:pt-0 last:pb-0"
-    >
-      <router-link
-        :to="{ name: 'questions.single', params: { id: post.id } }"
-        class="inline-block w-full hover:text-blueSky-dark-300"
-        >{{ post.title }}</router-link
+  <div v-if="authStore.isAuthenticated()">
+    <div class="grid grid-cols-1 divide-y divide-gray-100 divide-solid mb-3">
+      <div
+        v-for="post in posts"
+        v-bind:key="post._id"
+        class="py-4 first:pt-0 last:pb-0"
       >
+        <router-link
+          :to="{ name: 'questions.single', params: { id: post._id } }"
+          class="inline-block w-full hover:text-blueSky-dark-300"
+          >{{ post.title }}</router-link
+        >
+      </div>
     </div>
+    <!-- <div class="text-blue">See all</div> -->
   </div>
-  <div class="text-blue">See all</div>
+  <div v-else>
+    Please <a href="/login" class="text-blueSky-dark-300">login</a> or
+    <a href="/sign-up" class="text-blueSky-dark-300">register</a> to see your
+    history.
+  </div>
 </template>
 
 <script setup>
-const historyPosts = [
-  { id: 1, title: "How does AWS work?" },
-  { id: 2, title: "Is Java better than C++?" },
-  { id: 3, title: "How much time to learn a programming language?" },
-];
+import { useUserStore } from "@/stores/user.js";
+import { useAuthStore } from "@/stores/auth.js";
+import { watchEffect, ref } from "vue";
+
+const userStore = useUserStore();
+const authStore = useAuthStore();
+const posts = ref();
+
+watchEffect(() => {
+  if (authStore.isAuthenticated()) {
+    var tempArr = userStore.user.history_posts.slice();
+    posts.value = tempArr.reverse().slice(0, 10);
+  }
+});
 </script>
 
 <style lang="scss" scoped></style>
